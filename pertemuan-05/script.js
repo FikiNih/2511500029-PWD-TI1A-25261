628584616873b2c965dfd1f679419d1f574f004b
FikiNih <2511500029@mahasiswa.atmaluhur.ac.id>
2025-10-27 14:55:36 +0700

    update

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,3 +1,5 @@
 Document.getELementById("menuToggle").addEventListener("click", function() {
     document.querySelector("nav").classList.toggle("active");
+    console.log("aku pria sigma");
+    document.getElementById(("txtnama"))
 });
