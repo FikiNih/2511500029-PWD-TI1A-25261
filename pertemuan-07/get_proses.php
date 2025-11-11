88feaadcfab8aee4869fd25db6676df0bf2e55d6
FikiNih <2511500029@mahasiswa.atmaluhur.ac.id>
2025-11-11 21:26:19 +0700

    menampilkan session dari get_proses.php ke get.php

--- pertemuan-07/get_proses.php
+++ pertemuan-07/get_proses.php
@@ -1,5 +1,6 @@
 <?php
-echo $_GET["txtNama"];
-echo $_GET["txtEmail"];
-echo $_GET["txtPesan"];
+session_start();
+$_SESSION["nama"] = $_GET["txtNama"];
+$_SESSION["email"] = $_GET["txtEmail"];
+$_SESSION["pesan"] = $_GET["txtPesan"];
 ?>
